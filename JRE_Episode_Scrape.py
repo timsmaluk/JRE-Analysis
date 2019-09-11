02e593115e6500a241c1e892af80d53df88207b0
Tim Smaluk <timsmaluk@gmail.com>
2019-09-10 18:37:00 -0700

    Adding csv outputs & new functions

--- JRE_Episode_Scrape.py
+++ JRE_Episode_Scrape.py
@@ -6,15 +6,16 @@ Created on Sat Aug 31 17:12:21 2019
 @author: Tim_Smaluk
 """
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 import urllib.parse
-import pandas
 import re
 
 guests = []
 urls = []
 full_list = []
 list_of_books = []
+episode_number_guest = []
 
 def url_connection(url):
     """
@@ -65,20 +66,41 @@ def scrape_books(url_list):
     Returns list of tuples of books mentioned and guest who mentioned it
     :@return(list):list of tuples (book,author) 
     """
-
-    result = url_connection(url_list[1]) #prints the first link for testing
-    x = result.find_all("h3", {"class" : "book-title"})
-    for a in x:
-        list_of_books.append(tuple((a.text).replace('\n', '').rsplit('by ', 1)
-        ))
+    for url in url_list:
+        result = url_connection(url) #prints the first link for testing
+        x = result.find_all("h3", {"class" : "book-title"})
+        for a in x:
+            list_of_books.append(tuple((a.text).replace('\n', '').rsplit('by ', 1)
+            ))
     return list_of_books
+
+
+def exporting_to_csv(names, books):
+    """
+    Creates a csv file from a Pandas Data Frame
+    :@return(file): csv file containing  Episode #, Name of Guest,Books, Author
+    """
+    try:
+        for name in names:
+            guest = tuple(name.split(' – ', 1))
+            episode_number_guest.append(guest)
+    except IndexError:
+        print(str(name))
+        pass    
     
+    df1 = pd.DataFrame(episode_number_guest, columns= ['Epsiode #', 'Name of Guest'])
+    df2 = pd.DataFrame(books, columns = ['Book Title', 'Book Author'])
+    return df1.to_csv(r'/Users/Tim_Smaluk/Desktop/JRE-Analysis/out2.csv',index=False, encoding='utf-8')
+    #return df2.to_csv(r'/Users/Tim_Smaluk/Desktop/JRE-Analysis/out2.csv',index=False, encoding='utf-8')
+
 def main():  
     seed = 'https://jrelibrary.com/episode-list/'
     soup = url_connection(seed)
     names, links = scrape_names(soup)
     url_list = cleanup_links(links)
-    print(scrape_books((url_list)))
+    books = scrape_books(url_list)
+    exporting_to_csv(names, books)
+
 
 if __name__ == "__main__":
      main()
